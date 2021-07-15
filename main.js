864c0b64d1618b9c01029fa9e7c647166e2685a0
Zaynthecoder <68971876+Zaynthecoder@users.noreply.github.com>
2021-07-15 16:58:52 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,106 @@
+//Create a reference for canvas 
+canvas = document.getElementById('myCanvas');
+ctx = canvas.getContext("2d");
+
+//Give specific height and width to the car image
+car_width = 130;
+car_height = 220;
+background_image = "parkingLot.jpg";
+ferrari_image = "car.png";
+
+//Set initial position for a car image.
+car_x = 5;
+car_y = 365;
+function add() {
+	//upload car, and background images on the canvas.
+	car_imgTag = new Image(); 
+	car_imgTag.onload = uploadcar; 
+	car_imgTag.src = ferrari_image;
+	
+	background_imgTag = new Image(); 
+	background_imgTag.onload = uploadBackground;
+	background_imgTag.src = background_image; 
+}
+
+function uploadBackground() {
+	ctx.drawImage(background_imgTag, 0, 0, canvas.width, canvas.height);
+}
+
+function uploadcar() {
+	ctx.drawImage(car_imgTag, car_x, car_y, car_width, car_height);
+}
+
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+		if(keyPressed == '38')
+		{
+			up();
+			console.log("up");
+		}
+	
+		if(keyPressed == '40')
+		{
+			down();
+			console.log("down");
+		}
+		
+		if(keyPressed == '37')
+		{
+			left();
+			console.log("left");
+		}
+	
+		if(keyPressed == '39')
+		{
+			right();
+			console.log("right");
+		}
+		
+		
+}
+
+function up()
+{
+	if(car_y >=0)
+	{
+		car_y = car_y - 10;
+		console.log("When up arrow is pressed,  x = " + car_x + " | y = " +car_y);
+		 uploadBackground();
+		 uploadcar();
+	}
+}
+function down()
+{
+	if(car_y <=500)
+	{
+		car_y =car_y+ 10;
+		console.log("When down arrow is pressed,  x = " + car_x + " | y = " +car_y);
+		uploadBackground();
+		 uploadcar();
+	}
+}
+function left()
+{
+	if(car_x >= 0)
+	{
+		car_x =car_x - 10;
+		console.log("When left arrow is pressed,  x = " + car_x + " | y = " +car_y);
+		uploadBackground();
+		 uploadcar();
+	}
+}
+function right()
+{
+	if(car_x <= 700)
+	{
+		car_x =car_x + 10;
+		console.log("When right arrow is pressed,  x = " + car_x + " | y = " +car_y);
+		uploadBackground();
+		uploadcar();
+   }
+}
